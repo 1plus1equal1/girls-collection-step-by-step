a7991d6f7c95f7fff1ade692725a4bd00fccbc6a
tsnanh <tsnanh0812@gmail.com>
2022-05-02 00:08:37 +0700

    init

---
+++ app/src/main/java/dev/tsnanh/android/girlscollection/SplashActivity.kt
@@ -0,0 +1,23 @@
+package dev.tsnanh.android.girlscollection
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        lifecycleScope.launch {
+            delay(5_000)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
